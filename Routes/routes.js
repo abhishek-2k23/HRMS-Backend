e8e3fa696b2cabf5fa671a20e90cff4d1ddeb026
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-12-16 23:23:42 +0530

    logout route and structured code

--- Routes/routes.js
+++ Routes/routes.js
@@ -9,13 +9,19 @@ import { login } from "../Controllers/login.js";
 import { bank } from "../Controllers/bank.js";
 import { getEmployeeDetails,getBankDetails } from "../Controllers/getdata.js";
 import { uploadToCloudinary } from "../Controllers/uploadpic.js";
+import { logout } from "../Controllers/logout.js";
 
-// //creating routes
+// //creating routes for user
 router.post("/register",register);
 router.post("/login",login);
-router.post("/addBankDetails",bank);
+router.get("/logout",logout);
 router.get("/getEmployeeDetails",getEmployeeDetails);
+
+//banking details
+router.post("/addBankDetails",bank);
 router.get("/getBankDetails",getBankDetails);
+
+//uploading pic
 router.post("/uploadPIC",uploadToCloudinary)
 
 
